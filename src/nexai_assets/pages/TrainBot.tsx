4dde589bb460d7044b2dda1f059eaebe1096b5f1
Abisuwa <ayodejiabisuwa23@gmail.com>
2023-04-19 10:31:59 +0100

    merged dev into main

---
+++ src/nexai_assets/pages/TrainBot.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Box} from "@chakra-ui/react"
+
+function TrainBot() {
+  return (
+    <Box>
+TRAINBOT
+    </Box>
+  )
+}
+
+export default TrainBot
--- src/nexai_assets/src/app.tsx
+++ src/nexai_assets/src/app.tsx
@@ -8,49 +8,51 @@ import LandingPage from "../pages/LandingPage";
 import Signup from "../pages/Signup";
 import Signin from "../pages/Signin";
 import Dashboard from "../pages/Dashboard";
+import TrainBot from "../pages/TrainBot";
 
 const App = () => {
-	// const { handleAuthenticated, setIIAuth } = useContext(AuthContext);
+	const { handleAuthenticated, setIIAuth } = useContext(AuthContext);
 
-	// const navigate = useNavigate();
+	const navigate = useNavigate();
 
-	// useEffect(() => {
-	// 	const runOnMounth = async () => {
-	// 		const authClient = await AuthClient.create();
-	// 		if (await authClient.isAuthenticated()) {
-	// 			// setTour(tour_);
-	// 			handleAuthenticated(authClient);
-	// 			if (location.pathname === "/") {
-	// 				navigate("/signup");
-	// 			}
-	// 			setIIAuth(true);
-	// 			setActorRestated(true);
-	// 		} else {
-	// 			toast.error("you must log in");
-	// 			navigate("/");
-	// 		}
-	// 	};
+	useEffect(() => {
+		const runOnMounth = async () => {
+			const authClient = await AuthClient.create();
+			if (await authClient.isAuthenticated()) {
+				// setTour(tour_);
+				handleAuthenticated(authClient);
+				if (location.pathname === "/") {
+					navigate("/signup");
+				}
+				setIIAuth(true);
+				setActorRestated(true);
+			} else {
+				toast.error("you must log in");
+				navigate("/");
+			}
+		};
 
-	// 	runOnMounth();
-	// }, []);
+		runOnMounth();
+	}, []);
 
-	// const [actorRestated, setActorRestated] = useState<boolean>(false);
+	const [actorRestated, setActorRestated] = useState<boolean>(false);
 
-	// if (actorRestated) {
-	// 	return (
-	// 		<Routes>
-	// 			<Route path='/signup' element={<Signup />} />
-	// 		</Routes>
-	// 	);
-	// } else {
+	if (actorRestated) {
+		return (
+			<Routes>
+				<Route path='/signup' element={<Signup />} />
+				<Route path='/train-bot' element={<TrainBot />} />
+			</Routes>
+		);
+	} else {
 		return (
 			<Routes>
 				<Route path='/' element={<LandingPage />} />
-						<Route path='/signup' element={<Signup />} />
+						{/* <Route path='/signup' element={<Signup />} /> */}
 
 			</Routes>
 		);
-	// }
+	}
 };
 
 export default App;
