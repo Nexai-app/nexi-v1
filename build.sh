e4e829f3880211d4963a52b413f8fceb771d326e
Success Aje <successaje7@gmail.com>
2024-04-23 11:02:11 +0100

    ckbtc changes

--- build.sh
+++ build.sh
@@ -8,7 +8,8 @@ dfx identity use minter
 MINT_ACC=$(dfx ledger account-id)
 export MINT_ACC
 
-dfx identity use default
+dfx identity use finisher
+
 LEDGER_ACC=$(dfx ledger account-id)
 export LEDGER_ACC
 
@@ -24,6 +25,34 @@ dfx deploy icp_ledger --argument "(variant {Init =record {minting_account = \"${
 initial_values = vec { record {  \"${LEDGER_ACC}\";
 record { e8s=100_000_000_000 } } } ; archive_options = opt record {num_blocks_to_archive = 1000000; trigger_threshold = 1000000; \
   controller_id = principal  \"${ARCHIVE_CONTROLLER}\"; }; send_whitelist = vec {}}})" --specified-id ryjl3-tyaaa-aaaaa-aaaba-cai
+
+export OWNER=$(dfx identity get-principal)
+
+dfx deploy --network local ckbtc_ledger --argument '
+  (variant {
+    Init = record {
+      token_name = "Local ckBTC";
+      token_symbol = "LCKBTC";
+      minting_account = record { owner = principal "'${OWNER}'";};
+      initial_balances = vec { record { record { owner = principal "'${OWNER}'";}; 100_000_000_000; }; };
+      metadata = vec {};
+      transfer_fee = 10;
+      archive_options = record {
+        trigger_threshold = 2000;
+        num_blocks_to_archive = 1000;
+        controller_id = principal "'${OWNER}'";
+      }
+    }
+  })
+'
+
+export LEDGER_PRINCIPAL=$(dfx canister --network local id ckbtc_index)
+
+dfx deploy --network local ckbtc_ledger --argument '
+  record {
+   ledger_id = (principal "'${LEDGER_PRINCIPAL}'");
+  }
+'
 
 dfx deploy icp_index --specified-id qhbym-qaaaa-aaaaa-aaafq-cai --argument '(record {ledger_id = principal "ryjl3-tyaaa-aaaaa-aaaba-cai"})'
 
